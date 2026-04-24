956d8eeacd796489f938b5574367cc29e8e2553e
pankaj <kali620162016201@gmail.com>
2026-04-24 10:30:46 +0530

    communication & modifie terraform and kubernetes

--- kubernetes/auto.sh
+++ kubernetes/auto.sh
@@ -1,7 +1,53 @@
 #!/bin/bash
 set -e
 
-echo "hello user all write "
-touch hello.txt
+echo "============================================"
+echo "Minikube Run and Coustom Image Deployment"
+echo "============================================"
 
-echo "i think file is created " 
+# set script write or not 
+set -e
+
+if ! command -v minikube &> /dev/null 
+then
+    echo "Minikube is Installing..."
+    curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64
+    sudo install minikube-linux-amd64 /usr/local/bin/minikube 
+    rm minikube-linux-amd64
+    echo "Minikube Install Successful..."
+else
+    echo "Minikube already Install"
+fi
+
+# Check minikube version 
+minikube version
+
+echo "============================================="
+echo "Minikube service status "
+echo "============================================="
+
+if minikube status | grep -q "Running"; then
+    echo "Minikube are running "
+else
+    minikube start --cpus=2 --memory=4096
+    echo "Minkkube Start and Running Know..."
+fi
+
+echo "Cluster information Check"
+kubectl get nodes
+
+echo "Deploy Application..."
+kubectl apply -f configmap.yaml
+kubectl apply -f deployment.yaml
+kubectl apply -f service.yaml
+
+echo "Waiting for pods to be ready..."
+sleep 10
+
+echo "pods are Information check "
+kubectl get pods
+
+echo "Click url your application live or not"
+minikube service my-webapp --url
+
+echo "all process complete successful..."
